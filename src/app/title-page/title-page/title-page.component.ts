bf1aed13fe36621fdbfd56d81f483b3325d31bff
Apoorv Anupam <apoorvanupam11@gmail.com>
2022-03-22 19:55:52 +0530

    made title page canvas size to fill window

--- src/app/title-page/title-page/title-page.component.ts
+++ src/app/title-page/title-page/title-page.component.ts
@@ -10,14 +10,19 @@ import * as PIXI from 'pixi.js'
 export class TitlePageComponent implements OnInit {
 
   private app: PIXI.Application = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: 500,
+    height: 500,
+    antialias: true
   });
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.app.renderer.resize(window.innerWidth, window.innerHeight);
     this.app.renderer.backgroundColor = 0x040A42;
+    this.app.renderer.view.style.position = 'absolute';
+
 
     document.body.appendChild(this.app.view);
   }
